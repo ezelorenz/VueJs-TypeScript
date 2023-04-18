a4e7e3542b97aed80645d97a5dd9ab312267059e
Ezequiel Lorenz <elorenz@bitwise.com.ar>
2023-04-18 10:21:47 -0300

    Inicializacion de proyecto

---
+++ 01-breakingBad/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>Hola </h1>
+    <p>Mundo</p>
+  </div>
+</template>
+
+<style scoped>
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
