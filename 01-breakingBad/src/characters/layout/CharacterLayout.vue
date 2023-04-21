d8cc8c906bbd120ef834670af1ade3fed821a820
Ezequiel Lorenz <elorenz@bitwise.com.ar>
2023-04-21 07:32:17 -0300

    Agregado de navBar secundario

--- 01-breakingBad/src/characters/layout/CharacterLayout.vue
+++ 01-breakingBad/src/characters/layout/CharacterLayout.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
+import { RouterView } from 'vue-router';
+import type { RouterLink} from '@/router/link-routes'
+import NavBar from '../../shared/components/NavBar.vue';
+import { characterRoute } from '../router/index';
 
+    const routeLinks: RouterLink[] = characterRoute.children!
+        .filter( route => (route.props as {visible: boolean}).visible)
+        .map(route =>{
+            return{
+                name: route.name as string,
+                path: route.path,
+                title: (route.props as {title: string, visible: boolean}).title,
+            }
+        });
 </script>
 
 <template>
-    <div>
-        <h1>Character Layout</h1>
-    </div>
+    <h1>Personajes</h1>
+
+    <!--NavBar-->
+    <NavBar 
+        :show-icon="false"
+        :links="routeLinks"
+    />
+
+    <!--RouterView + Suspense-->
+    
+    <RouterView/>
 </template>
 
 
